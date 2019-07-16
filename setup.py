484585ee5842834b7645f5844c53082e0af769ef
yimian_sufagang <sufagang@yimian.com.cn>
2019-07-16 16:37:11 +0800

    change version tag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='curd',
-    version='0.0.11',
+    version='0.0.12',
     url='https://github.com/sfgxuan/curd',
     description='db operations',
     author='jdxin0',
